e890549d8a52a4806dac9ce9f26062a7cf8d2f2a
Pedro Machado <pedromara2709@gmail.com>
2026-08-20 23:56:24 -0300

    exercicio 02

---
+++ Relatorio 02/Exercicio 02.lua
@@ -0,0 +1,32 @@
+local tabela= {}
+local cont = 0
+
+local function contarOcorrencias(tabela, alvo)
+    for i=1, #tabela do
+    if tabela[i] == alvo then 
+      cont = cont + 1
+    end
+  end  
+
+  return cont
+
+  
+end
+
+print("Insira a quantidade de elementos:")
+local elementos = tonumber(io.read())
+
+for i = 1, elementos do
+  print("digite o elemento " .. i)
+  table.insert(tabela, tonumber(io.read()))
+  end 
+
+
+print("Insira um número a ser buscado")
+local procurado = tonumber(io.read())
+
+local ocorrencias = contarOcorrencias(tabela, procurado)
+
+if ocorrencias ~= 0 then 
+    print("O numero " .. procurado .. " aparece " .. ocorrencias .. " vez(es) na tabela")
+  end
