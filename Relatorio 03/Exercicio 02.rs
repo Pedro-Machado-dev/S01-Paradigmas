85b3613491998bf3ac372602d63e8c32c17c6a7d
Pedro Machado <pedromara2709@gmail.com>
2026-08-28 21:49:38 -0300

    exercicio 02

---
+++ Relatorio 03/Exercicio 02.rs
@@ -0,0 +1,34 @@
+use std::io::{self, Write};
+
+fn acertou_o_alvo(palpite: i32, numero_secreto: i32) -> bool {
+    (palpite - numero_secreto).abs() <= 5
+}
+
+fn main() {
+    let numero_secreto: i32 = 27;
+
+    loop {
+        print!("Digite seu palpite: ");
+        io::stdout().flush().unwrap();
+
+        let mut entrada = String::new();
+        let lidos = io::stdin().read_line(&mut entrada).unwrap();
+
+        if lidos == 0 {
+            break;
+        }
+
+        let palpite: i32 = match entrada.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if acertou_o_alvo(palpite, numero_secreto) {
+            let distancia = (palpite - numero_secreto).abs();
+            println!("Voce acertou! Ficou a apenas {} unidades do numero secreto!", distancia);
+            break;
+        } else {
+            println!("Voce! passou longe Tente novamente.");
+        }
+    }
+}
